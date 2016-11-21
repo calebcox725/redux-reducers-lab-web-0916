8603814238bb7f8d3022c0c42980b46f629e1470
calebcox725 <caleb.cox@flatironschool.com>
2016-11-21 18:21:49 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,12 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+    case 'REMOVE_INGREDIENT':
+      let foo =  [...state]
+      foo.splice(action.payload, 1)
+      return foo
+    default:
+      return state
+  }
 }
